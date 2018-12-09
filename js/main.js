8afcd01ece212dc54a7c5f8ea1d1650384ddfbf8
JoseDiegoMarinContreras <josepancho_marin@hotmail.com>
2018-12-09 12:31:37 -0700

    Commit con opcion de filtrar usando el boton y dando enter

--- js/main.js
+++ js/main.js
@@ -1,140 +1,69 @@
-var dataAlumnos = [
-    {
-        No_Control: 14400961,
-        Nombres: "Jose Diego",
-        Apellidos: "Marin Contreras",
-        Telefono: "311-847-10-47",
-        Materias: [
-            {
-                Codigo_Materia: "SCC-1019",
-                Nombre_Materia: "Programacion logica y funcional",
-                Calificacion: 90
-            },
-            {
-                Codigo_Materia: "SCP-1012",
-                Nombre_Materia: "Aplicaciones empresariales",
-                Calificacion: 91
-            }
-        ]
-    },
-    {
-        No_Control: 14400365,
-        Nombres: "Agustin",
-        Apellidos: "Barajas Valdivia",
-        Telefono: "322-156-82-96",
-        Materias: [
-            {
-                Codigo_Materia: "SCC-1019",
-                Nombre_Materia: "Programacion logica y funcional",
-                Calificacion: 93
-            },
-            {
-                Codigo_Materia: "SCP-1012",
-                Nombre_Materia: "Aplicaciones empresariales",
-                Calificacion: 95
-            }
-        ]
-    },
-    {
-        No_Control: 14400119,
-        Nombres: "Irving Yair",
-        Apellidos: "Nava Hernandez",
-        Telefono: "311-174-25-19",
-        Materias: [
-            {
-                Codigo_Materia: "ACF-0904",
-                Nombre_Materia: "Calculo Vectorial",
-                Calificacion: 80
-            },
-            {
-                Codigo_Materia: "SCP-1010",
-                Nombre_Materia: "NoSQL",
-                Calificacion: 94
-            },
-            {
-                Codigo_Materia: "SCC-1019",
-                Nombre_Materia: "Programacion logica y funcional",
-                Calificacion: 92
-            },
-            {
-                Codigo_Materia: "SCC-1013",
-                Nombre_Materia: "Investigacion de operaciones",
-                Calificacion: 70
-            }
-        ]
-    },
-    {
-        No_Control: 14400666,
-        Nombres: "Hector Ivan",
-        Apellidos: "Herrera Barragan",
-        Telefono: "311-169-34-53",
-        Materias: [
-            {
-                Codigo_Materia: "SCP-1666",
-                Nombre_Materia: "Aplicaciones multiplataforma",
-                Calificacion: 81
-            },
-            {
-                Codigo_Materia: "SCD-1021",
-                Nombre_Materia: "Redes de computadora",
-                Calificacion: 77
-            },
-            {
-                Codigo_Materia: "SCP-1010",
-                Nombre_Materia: "NoSQL",
-                Calificacion: 100
-            },
-            {
-                Codigo_Materia: "SCA-1026",
-                Nombre_Materia: "Taller de sistemas operativos",
-                Calificacion: 100
-            },
-            {
-                Codigo_Materia: "SCC-1019",
-                Nombre_Materia: "Programacion logica y funcional",
-                Calificacion: 90
-            }
-        ]
-    }
-];
+var dataTablaAlumnos;
 
 document.addEventListener("DOMContentLoaded", function() {
     cargarDatosTabla();
+
+    //=============== EVENTOS ===============
+    let realizarBusqueda = () => {
+        let opcion = $("#cmbTipFiltro").val();
+        if(opcion == 0){
+            Swal({
+                type: 'error',
+                title: 'Error',
+                text: 'Opcion de filtro no permitido'
+            });
+        }else{
+            filtrarDatos(dataTablaAlumnos, opcion, $("#valorFiltro").val());
+        }
+    };
+    
+    $("#btnFiltrar").on('click', realizarBusqueda);
+    $("#valorFiltro").on('keyup', (evt) => {
+        if(evt.which == 13){
+            realizarBusqueda();
+        }
+    });
 });
 
 function cargarDatosTabla(){
     fetchDatAlumnos();
 }
+
 function fetchDatAlumnos(){
-    //fetch("URL")
-    //    .then(response => response.json())
-    //        .the((dataAlumnos) => {
-                procesarDatosAlumnos(dataAlumnos)
-    //        });
+    fetch("https://app-calificaciones.herokuapp.com/plf/alumnos")
+        .then(response => response.json())
+            .then((dataAlumnosJson) => procesarDatosAlumnos(dataAlumnosJson));
 }
+
 function procesarDatosAlumnos(rawDataAlumns){
-    let alumnosResult=[];
+    dataTablaAlumnos=[];
     _.forEach(rawDataAlumns, function(alumno){
         _.map(alumno.Materias, function(materia){
-            let wrapped = _(alumnosResult).push({
-                NoControl: alumno.No_Control,
-                NombCompleto: alumno.Apellidos + " " + alumno.Nombres,
-                Materia: materia.Nombre_Materia,
+            let wrapped = _(dataTablaAlumnos).push({
+                No_Control: alumno.No_Control,
+                NombreCompleto: alumno.Apellidos + " " + alumno.Nombres,
+                Nombre_Materia: materia.Nombre_Materia,
                 Calificacion: materia.Calificacion
             });
             wrapped.commit();
         });
     });
-    insertDatosTablaAlmns(alumnosResult)
-    console.log(alumnosResult);
+    insertDatosTablaAlmns(dataTablaAlumnos);
 }
+
 function insertDatosTablaAlmns(dataAlumns){
+    let cuerpoTabla = $("#tablaCalf tbody");
+    cuerpoTabla.html("");
     _.forEach(dataAlumns, function(alumno){
-        $("#tablaCalf tbody").append(`<tr>
-            <td>${alumno.NoControl}</td>
-            <td>${alumno.NombCompleto}</td>
-            <td>${alumno.Materia}</td>
+        cuerpoTabla.append(`<tr>
+            <td>${alumno.No_Control}</td>
+            <td>${alumno.NombreCompleto}</td>
+            <td>${alumno.Nombre_Materia}</td>
             <td>${alumno.Calificacion}</td>
         </tr>`);
     });
+}
+
+function filtrarDatos(data, filtro, valorFiltro){
+    insertDatosTablaAlmns(_.filter(data, (alumno) => String(alumno[filtro]).toLowerCase().includes(valorFiltro.toLowerCase())));
 }
